fca58d3fbba3a2b6c09d2d02bfd5d9c1590c99ae
Choi, MinJi <mamber0243@naver.com>
2024-09-25 03:40:42 +0900

    최초 커밋

---
+++ src/git_test4/Hello.java
@@ -0,0 +1,6 @@
+package git_test4;
+public class Hello {
+public void say() {
+System.out.println("hello world");
+}
+}
---
+++ src/git_test4/Main.java
@@ -0,0 +1,6 @@
+package git_test4;
+public class Main {
+public static void main(String[] args) {
+new Hello().say();
+}
+}
